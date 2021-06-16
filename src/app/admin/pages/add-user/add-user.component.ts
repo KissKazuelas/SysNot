fdaed89f8f82fdd0e0c59ee6545ff7605c3af1d0
KissKazuelas <mrjorge221@gmail.com>
2021-06-15 22:35:53 -0500

    register

--- src/app/admin/pages/add-user/add-user.component.ts
+++ src/app/admin/pages/add-user/add-user.component.ts
@@ -20,7 +20,6 @@ export class AddUserComponent implements OnInit {
   })
     
 
-
   usuario: UserElement = {
     name: "",
     email: "",
@@ -42,12 +41,13 @@ export class AddUserComponent implements OnInit {
     if(errors?.required){
       return 'Telefono obligatorio';
     }else if( errors?.pattern){
-      return 'El telefono debe ser contener solo números';
+      return 'El telefono debe contener solo números';
     }else if(errors?.minlength){
       return 'Telefono demasiado corto';
     }
     return '';
   }
+
 
   msgs1!: Message[];
   constructor(private fb : FormBuilder,
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,13 +2,26 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import { UiElementsModule } from '../ui-elements/ui-elements.module';
+import { SharedModule } from '../shared/shared.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ResgiterComponent } from './pages/resgiter/resgiter.component';
+import { LoginComponent } from './pages/login/login.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ResgiterComponent,
+    LoginComponent
+  ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    UiElementsModule,
+    SharedModule,
+    FormsModule,
+    ReactiveFormsModule
+
   ]
 })
 export class AuthModule { }
